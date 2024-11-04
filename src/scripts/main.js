91648bf8c0fbb42dca5e4f3e0fcf16fae5dddfef
Ivan <fanmetalheavy@gmail.com>
2024-11-04 22:58:47 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
 // write your code here
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    const timeout = window.setTimeout(() => {
+      message.setAttribute('class', 'message error-message');
+      reject(new Error('Promise was rejected!'));
+
+      document.body.append(message);
+    }, 3000);
+
+    logo.addEventListener('click', (e) => {
+      if (e.target) {
+        resolve('Promise was resolved!');
+        document.body.append(message);
+        window.clearTimeout(timeout);
+      }
+    });
+  });
+}
+
+const logo = document.getElementsByClassName('logo')[0];
+const message = document.createElement('div');
+
+message.setAttribute('class', 'message');
+
+createPromise()
+  .then((success) => {
+    message.textContent = success;
+  })
+  .catch((err) => {
+    message.textContent = err;
+  });
