27900ef333d148084283cb03cb15dd180ec1c524
Ivan <fanmetalheavy@gmail.com>
2024-11-04 23:21:31 +0200

    add reduction performed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,31 @@
 'use strict';
 
 // write your code here
-function createPromise() {
-  return new Promise((resolve, reject) => {
-    const timeout = window.setTimeout(() => {
-      message.setAttribute('class', 'message error-message');
-      reject(new Error('Promise was rejected!'));
-
-      document.body.append(message);
-    }, 3000);
-
-    logo.addEventListener('click', (e) => {
-      if (e.target) {
-        resolve('Promise was resolved!');
-        document.body.append(message);
-        window.clearTimeout(timeout);
-      }
-    });
-  });
-}
-
 const logo = document.getElementsByClassName('logo')[0];
 const message = document.createElement('div');
 
 message.setAttribute('class', 'message');
+
+function createPromise() {
+  return new Promise((resolve, reject) => {
+    const timeout = window.setTimeout(() => {
+      message.classList.add('error-message');
+      reject(new Error('Promise was rejected!'));
+      document.body.append(message);
+    }, 3000);
+
+    logo.addEventListener('click', () => {
+      resolve('Promise was resolved!');
+      document.body.append(message);
+      clearTimeout(timeout);
+    });
+  });
+}
 
 createPromise()
   .then((success) => {
     message.textContent = success;
   })
   .catch((err) => {
-    message.textContent = err;
+    message.textContent = err.message;
   });
